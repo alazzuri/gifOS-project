b55da68a8cdcb3ac1af6310f740aef05195a6e9b
alazzuri <alexislazzuri@gmail.com>
2019-11-23 16:39:09 -0300

    Create api-giphy.js
    This js file will manage the integration with Giphy's API for purposes of searching GIFS and printing them into the HTML file.

---
+++ js/api-giphy.js
@@ -0,0 +1,54 @@
+
+// ASIGNAR ID EN HTML
+const $SEARCHBUTTON = document.querySelector("#search");
+// VALIDAR API
+const API_KEY = "eiVo3MScNwrZJfkUOIP0WHzIV8uOQesx";
+// TODO COORREGIR CON LA CLASE CORRECTA DEL SELECTOR - ASIGNAR EN HTML
+const $MAINCONTAINER = document.querySelector(".mainContainer");
+
+
+// EJECUTA FUNCION AL HACER CLICK AL BOTON DE BUSQUEDA - VER COMO HACER PARA QUE TOME EL ENTER COMO INPUT
+$SEARCHBUTTON.onclick = function () {
+    //Capturo INPUT - ASIGNAR ID EN EL HTML.
+    const userRequest = document.querySelector("#searchbar").value;
+    //LIMPIO ARRAY
+    cleanSearchHistory();
+    //EJECUTO BUSQUEDA E IMPRIMO
+    obtainUrls(userRequest);
+}
+
+// FUNCION PARA LIMPIAR EL ARRAY
+function cleanSearchHistory() {
+    const $GIFSTOREMOVE = document.querySelectorAll(".gifContainer")
+    $GIFSTOREMOVE.forEach($GIFSTOREMOVE => {
+        $GIFSTOREMOVE.remove()
+    });
+}
+
+// REQUEST AL API DE GIPHY
+function getSearchResults(request) {
+    const found = fetch("https:/api.giphy.com/v1/gifs/search?q=" + request + "&api_key=" + API_KEY)
+        .then((response) => {
+            return response.json()
+        });
+    return found
+}
+
+// TOMO EL RESPONSE DE GIPHY Y LLAMO A LA FUNCION PARA IMPRIMIR POR CADA ITERACION.
+// PASO URLS COMO PARAMETRO.
+const obtainUrls = async function sacarUrls(userRequest) {
+    const URLS = await getSearchResults(userRequest)
+    URLS.data.forEach(data => {
+        printGifs(data.images.original.url)
+    });
+}
+
+// FUNCION PARA IMPRIMIR LAS IMAGENES
+function printGifs(urlAImprimir) {
+    $CONTENEDORIMG = document.createElement("img");
+    $CONTENEDORIMG.className = "gifContainer";
+    $CONTENEDORIMG.src = `${urlAImprimir}`;
+    $MAINCONTAINER.appendChild($CONTENEDORIMG);
+}
+
+

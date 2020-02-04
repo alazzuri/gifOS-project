cf2dee8465889f2b65f7cecbc65ac811a716d20a
alazzuri <alexislazzuri@gmail.com>
2020-02-04 20:30:51 -0300

    Modifying Visit Counter(save visit in LS)

--- js/visit-counter.js
+++ js/visit-counter.js
@@ -1,8 +1,14 @@
 const $VISIT_COUNTER = document.querySelector("header");
-const randomNumber = Math.ceil(Math.random() * 10000000);
-const numberToPrint = formatNumber(randomNumber);
+const startNumber = 10000000;
+let previousVisit =
+  +localStorage.getItem("previous-visit") !== 0
+    ? localStorage.getItem("previous-visit")
+    : 1;
+const numberToPrint = formatNumber(startNumber + +previousVisit);
 function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
 }
 $VISIT_COUNTER.textContent = `¡Bienvenidos/as a Guifos.com! ——————Donde los gifs están.////// Número de
 visitas: ${numberToPrint}`;
+
+localStorage.setItem("previous-visit", `${previousVisit++}`);
